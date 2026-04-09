ed7f84efbaa4bcf2f1857775d702b4f5f1f151db
Saifullah Anik <sanik4144@gmail.com>
2026-04-09 23:20:24 +0600

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const analyzerHandler = require('./routeHandlers/analyzerHandler');
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/', analyzerHandler);
